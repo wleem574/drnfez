e86b8e9e016c3381ad02db191e24f113663cf0ba
wleem574 <tgrba574@gmail.com>
2024-12-02 09:13:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    // هنا الكود الخاص بك
+    const form = document.getElementById("loginForm");
+    if (!form) {
+        console.error("Form element not found!");
+    }
+});
